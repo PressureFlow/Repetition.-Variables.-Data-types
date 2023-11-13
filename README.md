4044c6906e177c14b8ff00ce68d98c07f0098cd6
Илья Самойлов <141013914+PressureFlow@users.noreply.github.com>
2023-11-13 22:20:33 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Repetition.-Variables.-Data-types
---
+++ Задание №2/main.js
@@ -0,0 +1,4 @@
+let name = "Ilya";
+alert( `hello ${1}`); // hello 1
+alert( `hello ${"name"}` ); // hello name (двойные кавычки ничего не делают)
+alert( `hello ${name} `); // hello Ilya
